de81fd787376b70c3ae7e616d34dcb74299f39e8
reayd-falmouth <dr323090@falmouth.ac.uk>
2025-05-22 19:42:33 +0100

    _gnbg

--- src/gnubg/__init__.py
+++ src/gnubg/__init__.py
@@ -1,7 +1,7 @@
 # gnubg/__init__.py
 
 # pull in all of your C++ module’s symbols
-from . import *
+from ._gnubg import *
 
 # optional: define __all__ if you want to tidy up the namespace
 __all__ = [name for name in globals().keys() if not name.startswith("_")]
