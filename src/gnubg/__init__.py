123ca4f79ab00424b11d1ad645989ab10d10c772
reayd-falmouth <dr323090@falmouth.ac.uk>
2025-05-23 06:52:36 +0100

    guard against non-existant directories

--- src/gnubg/__init__.py
+++ src/gnubg/__init__.py
@@ -2,11 +2,11 @@
 import os
 from pathlib import Path
 
-# On Windows Python 3.8+, extend the DLL search path
 if os.name == "nt" and hasattr(os, "add_dll_directory"):
     pkgdir = Path(__file__).parent
     libdir = pkgdir / ".gnubg.mesonpy.libs"
-    os.add_dll_directory(str(libdir))
+    if libdir.is_dir():  # only add if it really exists
+        os.add_dll_directory(str(libdir))
 
 # pull in all of your C++ module’s symbols
 from .gnubg import *
