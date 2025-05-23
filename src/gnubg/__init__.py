f3486a44bebdbbf41d31cb4b97e0016c339edc7c
reayd-falmouth <dr323090@falmouth.ac.uk>
2025-05-23 06:58:03 +0100

    disabled testing for pyd inspection

--- src/gnubg/__init__.py
+++ src/gnubg/__init__.py
@@ -1,12 +1,4 @@
 # gnubg/__init__.py
-import os
-from pathlib import Path
-
-if os.name == "nt" and hasattr(os, "add_dll_directory"):
-    pkgdir = Path(__file__).parent
-    libdir = pkgdir / ".gnubg.mesonpy.libs"
-    if libdir.is_dir():  # only add if it really exists
-        os.add_dll_directory(str(libdir))
 
 # pull in all of your C++ module’s symbols
 from .gnubg import *
