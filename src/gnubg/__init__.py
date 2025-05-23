434d87b7e51c0e5f578842fba4873c4765c05a95
reayd-falmouth <dr323090@falmouth.ac.uk>
2025-05-23 06:48:47 +0100

    updated with shared libs

--- src/gnubg/__init__.py
+++ src/gnubg/__init__.py
@@ -1,4 +1,12 @@
 # gnubg/__init__.py
+import os
+from pathlib import Path
+
+# On Windows Python 3.8+, extend the DLL search path
+if os.name == "nt" and hasattr(os, "add_dll_directory"):
+    pkgdir = Path(__file__).parent
+    libdir = pkgdir / ".gnubg.mesonpy.libs"
+    os.add_dll_directory(str(libdir))
 
 # pull in all of your C++ module’s symbols
 from .gnubg import *
