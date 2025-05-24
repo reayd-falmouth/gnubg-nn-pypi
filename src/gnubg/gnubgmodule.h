538b86ee5d0a9b465fbb190dd9a77d0c1b3dcae1
reayd-falmouth <dr323090@falmouth.ac.uk>
2025-05-24 06:19:39 +0100

    copyright

--- src/gnubg/gnubgmodule.h
+++ src/gnubg/gnubgmodule.h
@@ -1,4 +1,23 @@
-// gnubgmodule.h
+/*
+ * gnubgmodule.cpp / gnubgmodule.h
+ *
+ * Originally by Joseph Heled <joseph@gnubg.org>, 2000
+ * Adapted for Python 3 and Meson build system by David Reay <dr323090@falmouth.ac.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #ifndef GNUBGMODULE_H
 #define GNUBGMODULE_H
 
